eb403d7f438d690e38cdfcf372a67a5ddb7e1430
Rinataeva <rinataeva@gmail.com>
2024-11-10 21:40:23 +0300

    feature/pie-chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Chart } from "react-google-charts";
+import "./App.css";
+import { UserProfile } from "./UserProfile/UserProfile";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const data = [
+    ["Task", "Hours per Day"],
+    ["Work", 7],
+    ["Commute", 1],
+    ["Cook", 1],
+    ["Eat", 1],
+    ["Hang around", 1],
+    ["Study", 3.5],
+    ["Read/Watch", 1],
+    ["Scroll", 0.5],
+    ["Sleep", 8],
+  ];
 
+  const options = {
+    title: "My Daily Activities",
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <UserProfile />
+      <Chart
+        chartType="PieChart"
+        data={data}
+        options={options}
+        width={"120%"}
+        height={"800px"}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/UserProfile/UserProfile.jsx
@@ -0,0 +1,8 @@
+export function UserProfile(){
+    return (
+        <div className="UserProfile">
+            <h2>My Profile</h2>
+        </div>
+    );
+}
+
